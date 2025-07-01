253e23cef33a0ca58c068a8e2508ccfd631b0d6a
adithya230o <adihemchand@gmail.com>
2025-07-01 19:27:58 +0530

    Spring boot project initialised

---
+++ src/main/java/com/adithya/ai_summariser_microservice/AiSummariserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adithya.ai_summariser_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSummariserMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSummariserMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adithya/ai_summariser_microservice/AiSummariserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adithya.ai_summariser_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSummariserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
